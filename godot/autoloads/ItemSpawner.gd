052f7f7bf2e0583e45303e3f895e3244d0aafa77
ASTRALLIBERTAD <astrallibertad@gmail.com>
2026-06-03 04:12:29 +0800

    OFW balikabayan and chest added

--- godot/autoloads/ItemSpawner.gd
+++ godot/autoloads/ItemSpawner.gd
@@ -9,16 +9,16 @@ func _ready():
 func _on_food_ready(position: Vector2) -> void:
 	spawn_item("palay", position)
 func _on_balikbayan_box_dropped(position: Vector2) -> void:
-	spawn_item("balikbayan_box", position)
+	spawn_item("Objects/Box", position)
 
 func spawn_item(item_id: String, position: Vector2) -> void:
-	var path = "res://Collectibles/" + item_id + ".tscn"
+	var path = "res://Collectibles/" + item_id + ".scn"
 	if not ResourceLoader.exists(path):
 		push_error("ItemSpawner: scene not found: " + path)
 		return
 	var instance = load(path).instantiate()
 	instance.position = position
-	get_tree().current_scene.add_child(instance)
+	get_tree().root.get_child(0).add_child(instance)
 
 func spawn_piso(amount: int, position: Vector2) -> void:
 	var instance = load("res://Collectibles/coin.tscn").instantiate()
